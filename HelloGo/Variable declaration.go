9364a373f00bd7ec4641701915c12d00f20260ec
Gautham BA <gautham.bangalore@gmail.com>
2016-11-01 21:17:21 +0530

    Variable declaration - short way.

--- HelloGo/Variable declaration.go
+++ HelloGo/Variable declaration.go
@@ -2,7 +2,13 @@ package main
 import "fmt"
 
 func main() {
+	// Go automatically infers the type, so it's not mandatory to specify it.
 	var message string="hello go"
 	var a, b, c int
+
+	// Short way
+	x, y, z:=1, 2.5, 6
+
 	fmt.Println(message, a, b, c)
+	fmt.Println(x, y, z)
 }
