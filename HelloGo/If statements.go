ed2437f5ea0e1049c19e24955fbbf5ef00a043b6
Gautham BA <gautham.bangalore@gmail.com>
2016-11-14 23:03:10 +0530

    If statements.

--- HelloGo/If statements.go
+++ HelloGo/If statements.go
@@ -1,4 +1,27 @@
 package main
 
+import "fmt"
+
+type Printer func(string) ()
+type Salutation struct {
+	name     string
+	greeting string
+}
+
+func greet4(salutation Salutation, do Printer, isFormal bool) {
+	// If statement.
+	if isFormal {
+		do(salutation.name)
+	}
+	do(salutation.greeting)
+}
+
+func CreatePrintFunction1(custom string) (Printer) {
+	return func(s string) {
+		fmt.Println(s + custom)
+	}
+}
 func main() {
+	s := Salutation{"Ezio Auditore Da Firenze", "Salute"}
+	greet4(s, CreatePrintFunction1("!!!"), false)
 }
