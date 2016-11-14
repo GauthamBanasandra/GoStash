1d55eeceeb3702c8717dcb0b8257c4376d023665
Gautham BA <gautham.bangalore@gmail.com>
2016-11-14 23:33:39 +0530

    Switch statements.

--- HelloGo/If statements.go
+++ HelloGo/If statements.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Printer func(string) ()
 type Salutation struct {
@@ -17,19 +19,58 @@ func greet4(salutation Salutation, do Printer, isFormal bool) {
 	/* Embedding statements. The "prefix" is executed only once and its scope is confined to that line.
 	It's like the initialization done in a for loop.
 	 */
-	if prefix := "Mr. "; isFormal {
+	if prefix := GetPrefix(salutation.name); isFormal {
 		do(prefix + salutation.name)
 	} else {
 		do(salutation.greeting)
 	}
 }
 
+func GetPrefix(name string) (prefix string) {
+	// Switch - case.
+	switch name {
+	case "Bob":
+		prefix = "Mr. "
+		fallthrough // Presence of a "fallthrough" causes the next statement to be chosen.
+	case "Joe", "Amy": // Valid for "Joe" or "Amy" as input.
+		prefix = "Dr. "
+	case "Count":
+		prefix = "Sir "
+	default:
+		prefix = "Dude "
+	}
+
+	// Turning switch into an if-else kind of branch.
+	switch {
+	case name == "Bob":
+		prefix = "Mr. "
+	case len(name) == 4:
+		prefix = "Short name "
+	}
+	return
+}
+
+func TypeSwitchTest(x interface{}) {
+	// Switching based on type of the variable.
+	switch x.(type) {
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
+	case Salutation:
+		fmt.Println("Salutation")
+	default:
+		fmt.Println("unknown")
+	}
+}
 func CreatePrintFunction1(custom string) (Printer) {
 	return func(s string) {
 		fmt.Println(s + custom)
 	}
 }
 func main() {
-	s := Salutation{"Ezio Auditore Da Firenze", "Salute"}
-	greet4(s, CreatePrintFunction1("!!!"), false)
+	s := Salutation{"Amy1", "Salute"}
+	greet4(s, CreatePrintFunction1("!!!"), true)
+	fmt.Println("\nFiguring out the type")
+	TypeSwitchTest(s)
 }
