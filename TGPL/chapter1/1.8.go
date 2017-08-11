62f7e4aa45679507a7cb27927a46b436fcc72fe2
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-08-11 19:53:07 +0530

    Add 1.8

---
+++ TGPL/chapter1/1.8.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"net/http"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Nothing to download")
+		return
+	}
+
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Println(os.Stderr, "fetch: %v", err)
+			os.Exit(1)
+		}
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Println(os.Stderr, "copy: %v", err)
+			os.Exit(1)
+		}
+
+		resp.Body.Close()
+	}
+}
